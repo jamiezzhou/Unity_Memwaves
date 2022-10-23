89eef34f779cb7eaf89d33a31dbc4c34f92b8ab6
Jamie Zhou <96752523+jamiezzhou@users.noreply.github.com>
2022-10-22 22:52:54 -0500

    newscript

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.IO;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class Connect : MonoBehaviour
 {   
-    public int level;
+    public static int level;
+    public static int photo;
 
-    public GameObject Easy;
-    public GameObject Hard;
+    public Button Easy;
+    public Button Hard;
     public GameObject picture1;
     public GameObject picture2;
 
     private bool level1Clicked = false;
     private bool level2Clicked = false;
     private bool pictureClicked = false;
+
+    void Awake(){
+        DontDestroyOnLoad(photo);
+    }
 
     public void Start()
     {
@@ -25,78 +33,114 @@ public class Connect : MonoBehaviour
 
     }
 
-    // public void DisplayFunctions()
-    // {
-    //     Level();
-    // }
-
-
-
-
-    public void LoadGame(string input)
+    // Load pictures accordingly
+    public void LoadGame()
     {
-
-
-        if ((level1Clicked || level2Clicked) && !pictureClicked)
-        {
-            Picture();
+        photo = Int32.Parse(this.name.Substring(7));
+        Debug.Log(photo);
+        if(level1Clicked){
+            SceneManager.LoadScene("9Pieces");
         }
+
+        if(level2Clicked){
+            SceneManager.LoadScene("16Pieces");
+        }
+
+        // if ((level1Clicked || level2Clicked) && !pictureClicked)
+        // {
+        //     Picture();
+        // }
         // else
         // {
         //     DisplayFunctions();
         // }
-        if ((level1Clicked || level2Clicked) && pictureClicked)
-        {
-            SceneManager.LoadScene("16Pieces");
-        }
+        // if ((level1Clicked || level2Clicked) && pictureClicked)
+        // {
+        // }
 
     }
 
+    // public void Load2Game()
+    // {
+    //     photo = picture2.GetComponent<Image>();
+    //     if(level1Clicked){
+    //         SceneManager.LoadScene("9Pieces");
+    //     }
+
+    //     if(level2Clicked){
+    //         SceneManager.LoadScene("16Pieces");
+    //     }
+    // }
+
     public void Level1()
     {
-        if (!level1Clicked && !level2Clicked)
-        {
-            level1Clicked = true; //user select level
-            level = 9;
-        }
-        else if(level1Clicked)
-        {
-            level1Clicked = false;//if the level is already selected, unselect
-            level = 0;
-        }
+        level1Clicked = true;
+        Easy.interactable = false;
+        level = 9;
         picture1.SetActive(level1Clicked);//picture1 is activated if the level is clicked
-        picture2.SetActive(level1Clicked);//picture2 is activated if the level is clicked 
+        picture2.SetActive(level1Clicked);//picture2 is activated if the level is clicked
+
+        if(level2Clicked){
+            level2Clicked = false;
+            Hard.interactable = true;
+            // Easy.colors.normalColor = Color.gray;
+        }
+
+        // if (!level1Clicked && !level2Clicked)
+        // {
+        //     level1Clicked = true; //user select level
+        //     level = 9;
+        // }
+        // else if(level1Clicked)
+        // {
+        //     level1Clicked = false;//if the level is already selected, unselect
+        //     level = 0;
+        // }
+        // picture1.SetActive(level1Clicked);//picture1 is activated if the level is clicked
+        // picture2.SetActive(level1Clicked);//picture2 is activated if the level is clicked 
     }
 
     public void Level2()
     {
-        if (!level1Clicked && !level2Clicked)
-        {
-            level2Clicked = true; //user select level
-            level = 12;
-        }
-        else if(level2Clicked)
-        {
-            level2Clicked = false;//if the level is already selected, unselect
-            level = 0;
-        }
+        level2Clicked = true;
+        Hard.interactable = false;
+        level = 16;
         picture1.SetActive(level2Clicked);//picture1 is activated if the level is clicked
-        picture2.SetActive(level2Clicked);//picture2 is activated if the level is clicked 
+        picture2.SetActive(level2Clicked);//picture2 is activated if the level is clicked
+
+        if(level1Clicked){
+            level1Clicked = false;
+            Easy.interactable = true;
+            // Hard.colors.normalColor = Color.gray;
+        }
+
+        // if (!level1Clicked && !level2Clicked)
+        // {
+        //     level2Clicked = true; //user select level
+        //     level = 12;
+        // }
+        // else if(level2Clicked)
+        // {
+        //     level2Clicked = false;//if the level is already selected, unselect
+        //     level = 0;
+        // }
+        // picture1.SetActive(level2Clicked);//picture1 is activated if the level is clicked
+        // picture2.SetActive(level2Clicked);//picture2 is activated if the level is clicked 
     }
 
-    public void Picture()
-    {
-        if (!pictureClicked)
-        {
-            pictureClicked = true; //user select picture 
-        }
-        else
-        {
-            pictureClicked = false;
+    // public void Picture()
+    // {
+    //     if (!pictureClicked)
+    //     {
+    //         pictureClicked = true; //user select picture 
+    //     }
+    //     else
+    //     {
+    //         pictureClicked = false;
             
-        }
+    //     }
 
-    }
+    // }
 }
 
 
--- Assets/Scripts/ScriptCalculation.cs
+++ Assets/Scripts/ScriptCalculation.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.IO;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ScriptCalculation : MonoBehaviour
 {
     // Start is called before the first frame update
     // private float
+    public int puzzlePhoto = Connect.photo;
 
     private float startTime;
     public static int count;
@@ -14,6 +18,8 @@ public class ScriptCalculation : MonoBehaviour
     public float scoreCal;
     void Start()
     {
+        Debug.Log(Connect.photo);
+        // SetPuzzlePhoto(Connect.photo);
         startTime = 0;
         count = 0;
     }
@@ -35,4 +41,14 @@ public class ScriptCalculation : MonoBehaviour
         yield return new WaitForSeconds(t);
         SceneManager.LoadScene("FinishGame");
     }
+
+    // int pieceNum = Connect.level;
+    // public void SetPuzzlePhoto(Image photo){
+    //     // GameObject[] Pieces_;
+    //     for (int i = 0; i < pieceNum; ++i){
+    //         GameObject.Find("puzzle_" + i);
+    //         GetComponent<SpriteRenderer>().sprite = photo.sprite;
+
+    //     }
+    // }
 }
